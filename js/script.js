d6ddb0252a5f14016cffae8fe147611fbc0f4a0c
sindetisarah <sindetisara@gmail.com>
2021-03-16 13:07:10 +0300

    added a javascript function onclicking venture

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-}
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
+function submition() { 
+  alert("Ready for Adventure");
 }
